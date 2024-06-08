e2f1d241bee0e0558b4b4902837cfda1903ccfd1
otorne <olaf.torne@gmail.com>
2024-06-08 12:47:55 +0700

    assignment 8

--- blogging/views.py
+++ blogging/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    queryset = queryset.order_by('-published_date')
+    model = Post
+    template_name = 'blogging/list.html'
+
+
+class PostDetailView(DetailView):
+
+    model = Post
+    template_name = 'blogging/detail.html'
 
 
 def list_view(request):
--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polling/', include('polling.urls')),
+    path('blogging/', include('blogging.urls')),
     path('', include('blogging.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
