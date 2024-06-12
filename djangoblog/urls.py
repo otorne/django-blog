bdb772aac8dcc7dafab974c2038246319bfbeb4b
otorne <olaf.torne@gmail.com>
2024-06-12 11:52:47 +0800

    applied black

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polling/', include('polling.urls')),
-    path('blogging/', include('blogging.urls')),
-    path('', include('blogging.urls')),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path("admin/", admin.site.urls),
+    path("polling/", include("polling.urls")),
+    path("blogging/", include("blogging.urls")),
+    path("", include("blogging.urls")),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
